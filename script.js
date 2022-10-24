30a3410cbbfc778bbdc685ec424bd2647c3cc3a5
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-24 15:52:20 -0400

    i dont even know

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hey my application will run in this part")
+document.getElementById('contacts').style.color = "purple"
+
+const handleColorChange = () => {
+    console.log('changing color')
+}
+
+const handleAlert = () => {
+    alert('Alerting the class')
+}
